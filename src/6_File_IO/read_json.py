a3b582b67d2a518f1f8c8a9f885a9ee5201c8e5c
chiachin-yen <jason.melon@gmail.com>
2018-11-01 14:23:52 +0800

    Remove BOM of UTF-8 json

--- src/6_File_IO/read_json.py
+++ src/6_File_IO/read_json.py
@@ -1,6 +1,9 @@
 import json
 import os
 
-script_dir = os.path.abspath(__file__)
-with open(os.path.join(script_dir, 'sample.json'), 'r', encoding='utf-8') as fp:
+script_dir = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(script_dir, 'sample.json'),
+          'r', encoding='utf-8') as fp:
     json_reader = json.load(fp)
+
+print(json_reader['results'][0]['address_components'][3]['long_name'])
